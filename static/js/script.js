297e96ab6a0a246652a0a97cf57cf21c59367ef1
Luis Almeida <122276206+lra2001@users.noreply.github.com>
2025-07-02 21:00:20 +0100

    add bootstrap
    - add bootstrap
    - tidy up css
    - add more content to all pages

--- static/js/script.js
+++ static/js/script.js
@@ -24,3 +24,26 @@ if (slides.length > 0) {
         setTimeout(showSlides, 3000); // Change image every 3 seconds
     }
 }
+
+// Check if the form with id 'travelForm' exists before adding event listener
+const travelForm = document.getElementById('travelForm');
+if (travelForm) {
+    travelForm.addEventListener('submit', function (e) {
+        e.preventDefault();
+        const name = document.getElementById('favoriteDestination').value;
+        const reason = document.getElementById('whyFavorite').value;
+        const motivation = document.getElementById('tripMotivation').value;
+        document.getElementById('formMessage').textContent = `Thanks for sharing! We are delighted ${name} was your favorite destination! It is an impressive destination and we can understand why you loved it, based on your answer "${reason}". We hope History Explorer can match your expectations and provide you "${motivation}"`;
+        this.reset();
+    });
+}
+
+// Random destination selector
+const randomDestination = document.getElementById('travelForm');
+if (randomDestination) {
+document.getElementById('randomDestination').addEventListener('click', () => {
+    const destinations = ['Tokyo', 'Paris', 'Sidney', 'New York', 'Rio de Janeiro', 'Cairo', 'Stockholm', 'Machu Picchu', 'Budapest', 'Lisbon', 'London', 'Karachi', 'Washington', 'Cape Town'];
+    const random = destinations [Math.floor(Math.random() * destinations.length)];
+    document.getElementById('randomDestinationResult').textContent = `Congratulations, prepare for your next trip to ${random}!`;
+});
+}
